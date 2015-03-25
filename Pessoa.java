f56f23d1ed65288e3b699d107fb4c810b7ea6df1
Roberto Dantas <7robertodantas@gmail.com>
2015-03-25 01:50:50 -0300

    LocalDate

--- Pessoa.java
+++ Pessoa.java
@@ -1,13 +1,16 @@
 import java.util.Calendar;
-import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.ZoneId;
+import java.time.Instant;
+import java.time.Period;
 import java.util.Date;
 /**
  * Write a description of class Pessoa here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Gabriela Cavalcante da Silva , Gustavo Alves , Roberto Dantas.
+ * @version 1.00
  */
 public class Pessoa
 {
@@ -24,8 +27,8 @@ public class Pessoa
     {
         nome = nome_;
         nascimento.set(Calendar.YEAR, nascimento_.YEAR); 
-		nascimento.set(Calendar.MONTH, nascimento_.MONTH); 
-		nascimento.set(Calendar.DAY_OF_MONTH, nascimento_.DAY_OF_MONTH);
+        nascimento.set(Calendar.MONTH, nascimento_.MONTH); 
+        nascimento.set(Calendar.DAY_OF_MONTH, nascimento_.DAY_OF_MONTH);
         CPF = CPF_;
         telefone = telefone_;
     } 
@@ -91,8 +94,8 @@ public class Pessoa
      */
     public void setNascimento(Calendar c) {
         nascimento.set(Calendar.YEAR, c.YEAR); 
-		nascimento.set(Calendar.MONTH, c.MONTH); 
-		nascimento.set(Calendar.DAY_OF_MONTH, c.DAY_OF_MONTH);
+        nascimento.set(Calendar.MONTH, c.MONTH); 
+        nascimento.set(Calendar.DAY_OF_MONTH, c.DAY_OF_MONTH);
     }
     
     /**
@@ -123,10 +126,13 @@ public class Pessoa
     public int getIdade() {
         Calendar c = Calendar.getInstance();    
         
-        LocalDate aniv = LocalDate.of(aniversario.YEAR, aniversario.MONTH, aniversario.DAY_OF_MONTH);
+        /* LocalDate aniv = LocalDate.of(aniversario.YEAR, aniversario.MONTH, aniversario.DAY_OF_MONTH);
         LocalDate hoje = LocalDate.now();
-         
-        Period periodo = Period.between(homemNoEspaco, homemNaLua);
+        */ 
+       
+       LocalDate homemNoEspaco = LocalDate.of(1961, Month.APRIL, 12);
+       LocalDate homemNaLua = LocalDate.of(1969, Month.MAY, 25);
+        Period periodo = Period.between(homemNoEspaco, homemNaLua); 
          
         System.out.printf("%s anos, %s mês e %s dias", 
         periodo.getYears() , periodo.getMonths(), periodo.getDays());
