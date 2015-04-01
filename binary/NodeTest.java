46b66b9eb1c02443665734bd0a37e43d0d291ca9
i-am-gabi <gabicavalcantesilva@gmail.com>
2015-04-01 11:53:41 -0300

    comentários

--- binary/NodeTest.java
+++ binary/NodeTest.java
@@ -8,8 +8,8 @@ import org.junit.Test;
 /**
  * The test class NodeTest.
  *
- * @author  (your name)
- * @version (a version number or a date)
+ * @author  Gabriela Cavalcante, Roberto Dantas
+ * @version 1.0
  */
 public class NodeTest
 {
--- binary/PessoaTest.java
+++ binary/PessoaTest.java
@@ -8,8 +8,8 @@ import org.junit.Test;
 /**
  * The test class PessoaTest.
  *
- * @author  (your name)
- * @version (a version number or a date)
+ * @author  Gabriela Cavalcante, Roberto Dantas
+ * @version 1.0
  */
 public class PessoaTest
 {
